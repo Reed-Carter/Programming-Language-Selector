3e2c7884bfdf3629426cba65fa0ec3c26c9109cf
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-21 12:59:26 -0800

    syle button and results section

--- js/scripts.js
+++ js/scripts.js
@@ -8,19 +8,19 @@ function determineLanguage(event) {
 
   let result;  
   if (age >= 50 && height <= 60 && eyeColor === "blue" && aspiration === "javaScript") {
-    result = "the programming language you should learn first is Java Script!";
+    result = "Java Script!";
   }
   else if (age < 50 && age >= 40 || height > 60 && height <= 63 || eyeColor === "Brown" && aspiration === "python") {
-    result = "the programming language you should learn first is Python";
+    result = "Python";
   }
   else if (age < 40 && age >= 30 || height > 63 && height <= 66 || eyeColor === "Brown" && aspiration === "java") {
-    result =  "the programming language you should learn first is Java";
+    result =  "Java";
   }
   else if (age < 30 && age >= 20 || height > 66 && height <= 69 || eyeColor === "Brown" && aspiration === "R") {
-    result =  "the programming language you should learn first is R";
+    result =  "R";
   }
   else if (age <20 || height > 69 || eyeColor === "Green" && aspiration === "cSharp") {
-    result =  "the programming language you should learn first is C#";
+    result =  "C#";
   }
   else {
     result = "Unfortunately based on your characteristics you shouldn't pick up coding. Maybe try hiking!";
