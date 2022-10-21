11e65d4a00e1c71f26614c3cc81c7e4d3b11dadf
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-21 09:24:01 -0800

    fix bug preventing variables to be indentified by linking scripts.js. add conditional statements for all possibilities

--- js/scripts.js
+++ js/scripts.js
@@ -2,14 +2,23 @@ function determineLanguage(event) {
   event.preventDefault();
   const age = parseInt(document.getElementById("inputAge").value)
   const height = parseInt(document.getElementById("inputHeight").value)
-  //const eyeColor = document.getElementById("inputEyeColor").value
+  const eyeColor = document.getElementById("inputEyeColor")
 
   let result;
   console.log("age" + age)
   console.log("heigt" + height)
   console.log("eyeColor" + eyeColor)
-  if (age >= 50 || height <= 65) {
+  if (age >= 50 || height <= 65 && eyeColor === "Blue") {
     result = "Rust";
+  }
+  else if (age < 50 && age > 30 || height > 65 && height < 72 && eyeColor === "Brown") {
+    result = "Python"
+  }
+  else if (age <= 30 || height >= 72 && eyeColor === "Green") {
+    result =  "Python"
+  }
+  else {
+    result = "unfortunately based on your characteristics you shouldn't pick up coding. Maybe try hiking!"
   }
 
   document.getElementById("output").innerText = result;
