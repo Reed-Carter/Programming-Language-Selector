644fccb463e60147ab4fe77c316d5c3d571f734d
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-21 10:06:28 -0800

    add fourth survey question 'aspirations' and linked to js submit functionality

--- js/scripts.js
+++ js/scripts.js
@@ -3,19 +3,27 @@ function determineLanguage(event) {
   const age = parseInt(document.getElementById("inputAge").value)
   const height = parseInt(document.getElementById("inputHeight").value)
   const eyeColor = document.getElementById("inputEyeColor")
+  const aspiration = document.getElementById("inputAspiration")
 
   let result;
   console.log("age" + age)
   console.log("heigt" + height)
   console.log("eyeColor" + eyeColor)
-  if (age >= 50 || height <= 65 && eyeColor === "Blue") {
-    result = "Rust";
+  console.log("aspiration" + aspiration)
+  if (age >= 50 || height <= 65 || eyeColor === "Blue" && aspiration === "javaScript") {
+    result = "Java Script";
   }
-  else if (age < 50 && age > 30 || height > 65 && height < 72 && eyeColor === "Brown") {
+  else if (age < 50 || age > 30 || height > 65 || height < 72 || eyeColor === "Brown" && aspiration === "python") {
     result = "Python"
   }
-  else if (age <= 30 || height >= 72 && eyeColor === "Green") {
-    result =  "Python"
+  else if (age <= 30 || height >= 72 || eyeColor === "Green" && aspiration === "java") {
+    result =  "Java"
+  }
+  else if (aspiration === "R") {
+    result = "R"
+  }
+  else if (aspiration === "cSharp") {
+    result = "C#"
   }
   else {
     result = "unfortunately based on your characteristics you shouldn't pick up coding. Maybe try hiking!"
