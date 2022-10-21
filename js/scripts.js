06b960ee7c26297834ad41b8c3a6eec9ce07fa00
Reed-Carter <114263922+Reed-Carter@users.noreply.github.com>
2022-10-21 12:02:08 -0800

    fix bug to return correct value

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,23 @@ function determineLanguage(event) {
   console.log("eyeColor" + eyeColor)
   console.log("aspiration" + aspiration)
   console.log("name" + name)
-  if (age >= 50 || height <= 60 || eyeColor === "blue" && aspiration === "javaScript") {
+  if (age >= 50 && height <= 60 && eyeColor === "blue" && aspiration === "javaScript") {
     result = "the programming languagee you should learn first is Java Script!";
   }
   else if (age < 50 && age >= 40 || height > 60 && height <= 63 || eyeColor === "Brown" && aspiration === "python") {
-    result = "Python";
+    result = "the programming languagee you should learn first is Python";
   }
   else if (age < 40 && age >= 30 || height > 63 && height <= 66 || eyeColor === "Brown" && aspiration === "java") {
-    result =  "Java";
+    result =  "the programming languagee you should learn first is Java";
   }
-  else if (age < 30 && age >= 20 || height > 66 && height <= 69|| eyeColor === "Brown" && aspiration === "R") {
-    result =  "R";
+  else if (age < 30 && age >= 20 || height > 66 && height <= 69 || eyeColor === "Brown" && aspiration === "R") {
+    result =  "the programming languagee you should learn first is R";
   }
   else if (age <20 || height > 69 || eyeColor === "Green" && aspiration === "cSharp") {
-    result =  "C#";
+    result =  "the programming languagee you should learn first is C#";
   }
   else {
-    result = "unfortunately based on your characteristics you shouldn't pick up coding. Maybe try hiking!";
+    result = "Unfortunately based on your characteristics you shouldn't pick up coding. Maybe try hiking!";
   }
 
   document.getElementById("output").innerText = result;
